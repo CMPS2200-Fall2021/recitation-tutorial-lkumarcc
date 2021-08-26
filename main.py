b7f71783cad7338e5b5f6cc7b6f1e7dbf6b79a76
lkumar837 <laya@aptsi.com>
2021-08-26 08:59:26 -0500

    Implement Sum of Squares

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def sum_of_squares(a):
-	pass
-#gbjknlm
+	sum = 0
+    for i in a:
+        mid = i * i
+        sum = sum + mid
+    pass
+
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
